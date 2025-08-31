8e0adb88e8191d435c33d2b11cfaea66bc474a04
Gad7203 <edonusec@gmail.com>
2025-08-31 21:52:50 +0000

    Update script.js
    Change placeholder

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-<script>
-  // Get lightbox elements
-  const lightbox = document.getElementById('lightbox');
-  const lightboxImg = document.getElementById('lightbox-img');
-  const closeBtn = document.querySelector('.lightbox .close');
-
-  // Open lightbox when clicking an image
-  document.querySelectorAll('.gallery img').forEach(img => {
-    img.addEventListener('click', () => {
-      lightbox.style.display = 'flex';
-      lightboxImg.src = img.src;
-    });
-  });
-
-  // Close when clicking X
-  closeBtn.addEventListener('click', () => {
-    lightbox.style.display = 'none';
-  });
-
-  // Close when clicking outside image
-  lightbox.addEventListener('click', (e) => {
-    if (e.target === lightbox) {
-      lightbox.style.display = 'none';
-    }
-  });
-</script>
 
 let slideIndex = 0;
 let slideTimer;
